1c1fd9478e0f330ab887a50d78328c96898beeee
Mike-EEE <michael@dragonspark.us>
2018-05-03 02:56:49 -0400

    Formatting code.

---
+++ StackOverflow.Performance.Delegates/DelegatePair.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace StackOverflow.Performance.Delegates
+{
+	public struct DelegatePair<TFrom, TTo>
+	{
+		DelegatePair(Func<TFrom, TTo> declared, Func<TFrom, TTo> compiled)
+		{
+			Declared = declared;
+			Compiled = compiled;
+		}
+
+		public DelegatePair(Func<TFrom, TTo> declared, Expression<Func<TFrom, TTo>> expression) :
+			this(declared, expression.Compile()) {}
+
+		public Func<TFrom, TTo> Declared { get; }
+
+		public Func<TFrom, TTo> Compiled { get; }
+	}
+}
---
+++ StackOverflow.Performance.Delegates/Delegates.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+
+namespace StackOverflow.Performance.Delegates
+{
+	[CoreJob, ClrJob, DisassemblyDiagnoser(true, printSource: true)]
+	public class Delegates
+	{
+		readonly DelegatePair<string, string> _empty;
+		readonly DelegatePair<string, int>    _expression;
+		readonly string                       _message;
+
+		public Delegates() : this(new DelegatePair<string, string>(_ => default, _ => default),
+		                          new DelegatePair<string, int>(x => x.Length, x => x.Length)) {}
+
+		public Delegates(DelegatePair<string, string> empty, DelegatePair<string, int> expression,
+		                 string message = "Hello World!")
+		{
+			_empty      = empty;
+			_expression = expression;
+			_message    = message;
+			EmptyDeclared();
+			EmptyCompiled();
+			ExpressionDeclared();
+			ExpressionCompiled();
+		}
+
+		[Benchmark]
+		public void EmptyDeclared() => _empty.Declared(default);
+
+		[Benchmark]
+		public void EmptyCompiled() => _empty.Compiled(default);
+
+		[Benchmark]
+		public void ExpressionDeclared() => _expression.Declared(_message);
+
+		[Benchmark]
+		public void ExpressionCompiled() => _expression.Compiled(_message);
+	}
+}
